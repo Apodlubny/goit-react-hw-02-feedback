cc57c97904c58a26e1ffa0663ac7b95f88f92ebf
Apodlubny <apodlubny@gmail.com>
2022-09-28 21:14:08 +0300

    main

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,15 @@
+import { NotificationMessage } from './Notification.styled';
+import { PropTypes } from 'prop-types';
+
+const Notification = ({ message }) => {
+  return <NotificationMessage>{message}</NotificationMessage>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+export default Notification;
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  font-size: 40px;
+  text-align: center;
+  color: #8d5402;
+`;
